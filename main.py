82328f4a080d189a39efd40bd36dfdaf5197a0fc
aryanchy451 <pht449@gmail.com>
2024-12-29 23:11:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -790,9 +790,9 @@ def youtube_link(url, message, ci, is_series=False, att=0,is_multi=False,has_drm
                   print("Error:", e)
                   return json.dumps({})
 
-              pssh_kid = extract_unique_pssh_and_kid(r.text)
+            pssh_kid = extract_unique_pssh_and_kid(r.text)
 
-              print(pssh_kid)
+            print(pssh_kid)
 
 
 
