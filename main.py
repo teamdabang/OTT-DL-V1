5c24cb6f8a059f49b129eb15101ac9a4c990761a
ChronosBots <nitinsahay2014@gmail.com>
2025-01-13 13:33:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1589,49 +1589,7 @@ def jiodl(client, message):
 
     print("[=>] Jio Cinema Playback And Button Created")
 
-from asyncio import create_subprocess_exec, create_subprocess_shell, run_coroutine_threadsafe
-from asyncio.subprocess import PIPE
-from pyrogram import Client
-from pyrogram.enums import ParseMode
-from pyrogram.types import InputMediaPhoto
-from pyrogram.errors import ReplyMarkupInvalid, FloodWait, PeerIdInvalid, ChannelInvalid, RPCError, UserNotParticipant, MessageNotModified, MessageEmpty, PhotoInvalidDimensions, WebpageCurlFailed, MediaEmpty
 
-
-#from asyncio import sleep
-
-from random import choice as rchoice
-from time import time
-from re import match as re_match
-
-
-async def sendFile(message, file, caption=None, buttons=None):
-    try:
-        return await message.reply_document(document=file, quote=True, caption=caption, disable_notification=True, reply_markup=buttons)
-
-    except Exception as e:
-       # LOGGER.error(str(e))
-        return str(e)
-
-async def sendMessage(message, text, buttons=None, photo=None, **kwargs):
-    try:
-        return await message.reply(text=text, quote=True, disable_web_page_preview=True, disable_notification=True,
-                                    reply_markup=buttons, reply_to_message_id=rply.id if (rply := message.reply_to_message) and not rply.text and not rply.caption else None,
-                                    **kwargs)
-
-    except Exception as e:
-     #   LOGGER.error(format_exc())
-        return str(e)
-
-async def cmd_exec(cmd, shell=False):
-    
-    if shell:
-        proc = await create_subprocess_shell(cmd, stdout=PIPE, stderr=PIPE)
-    else:
-        proc = await create_subprocess_exec(*cmd, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = await proc.communicate()
-    stdout = stdout.decode().strip()
-    stderr = stderr.decode().strip()
-    return stdout, stderr, proc.returncode
 
 #!/usr/bin/env python3
 from pyrogram.handlers import MessageHandler, EditedMessageHandler
