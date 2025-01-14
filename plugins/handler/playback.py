00f49397ab4b143e9edd41ce03d821d5f0b33129
ChronosBots <nitinsahay2014@gmail.com>
2025-01-14 22:08:24 +0530

    Update playback.py

--- plugins/handler/playback.py
+++ plugins/handler/playback.py
@@ -1,4 +1,27 @@
+import json
+import re
+import requests
+import utils
+import yt_dlp
+from plugins.jiodl import *
+#from plugins.gdrive import *
+from main import *
+import subprocess
+from pyrogram import Client, filters, idle
+from urllib import parse
+import logging
+import os
 
+from plugins.handler.mhandler import *
+from plugins.dl import *
+from plugins.exec import *
+from plugins.jio import *
+from plugins.dash import *
+
+from base64 import b64decode, b64encode
+from yt_dlp.postprocessor import PostProcessor
+from utils import scriptsDir, joinPath, realPath
+from asyncio import create_subprocess_exec, create_subprocess_shell, run_coroutine_threadsafe, sleep
 
 
 
