6967d400e2ed8cb73e02cff67305012e8a2bace4
ChronosBots <nitinsahay2014@gmail.com>
2025-01-14 23:55:51 +0530

    Update ytdl.py

--- plugins/ytdl.py
+++ plugins/ytdl.py
@@ -13,7 +13,7 @@ from urllib import parse
 import logging
 import os
 from plugins.handler.playback import *
-from plugins.button import *
+from plugins.buttons import *
 
 from plugins.handler.mhandler import *
 from plugins.dl import *
