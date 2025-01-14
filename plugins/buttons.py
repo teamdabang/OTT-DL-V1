30dbf87e867539f1d1ca3c857b18eea6796c4582
ChronosBots <nitinsahay2014@gmail.com>
2025-01-14 23:51:48 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -3,47 +3,13 @@ import re
 import requests
 import utils
 import yt_dlp
-from plugins.jiodl import *
-from plugins.gdrive import *
 import subprocess
 from pyrogram import Client, filters, idle
 from urllib import parse
 import logging
 import os
-from plugins.handler.playback import *
-from plugins.ytdl import *
-from plugins.handler.mhandler import *
-from plugins.dl import *
-from plugins.exec import *
-from plugins.jio import *
-from plugins.dash import *
 
-from base64 import b64decode, b64encode
-from yt_dlp.postprocessor import PostProcessor
-from utils import scriptsDir, joinPath, realPath
-from asyncio import create_subprocess_exec, create_subprocess_shell, run_coroutine_threadsafe, sleep
-#from button import ButtonMaker
-LOG_FILE = "log.txt"
-logging.basicConfig(
-    format="[%(asctime)s] [%(levelname)s] - %(message)s",
-    datefmt="%d-%b-%y %I:%M:%S %p",
-    level=logging.INFO,
-    handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler()]
-)
-def atos(func, *args, wait=True, **kwargs):
-    future = run_coroutine_threadsafe(func(*args, **kwargs), bot_loop)
-    return future.result() if wait else future
-import os
-#from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-configPath = joinPath(scriptsDir, 'config.json')
-if not utils.isExist(configPath):
-    utils.copyFile(joinPath(scriptsDir, 'config.def'), configPath)
 
-# Some important variables
-default_res = ""
-default_strm = ''
-config = utils.JSO(configPath, 4)
-sudo_users = [7126874550, -1002273935741, 6209057258, 1596559467, 7361945688, 1195351595]
 
 class ButtonMaker:
     def __init__(self):
