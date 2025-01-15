9c24d41f29c5b6ad08c06bc42c93b1ca2e4bbf70
TONY-STARK-MAHESH <tonystarkmahesh1995@gmail.com>
2025-01-15 15:01:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -308,7 +308,7 @@ def download_vod_ytdlp(url, message, content_id, user_id, is_multi=False, has_dr
         pass
     output_name += f'.{language}'
     output_name += '.WEB-DL-JC'
-    output_name += ".@aryanchy451"
+    output_name += ".@paypalmafiaofficial"
     
 
     # Audio Codec
